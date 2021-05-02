04298336649c073eea2ab6c0a2d90f8c20dd12a8
kazumasasama <takaik@yahoo.com>
2021-05-02 14:36:00 +0900

    the first commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+  namespace :api, defaults: { format: :json } do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+          registrations: 'api/v1/auth/registrations',
+          sessions: 'api/v1/auth/sessions',
+          passwords: 'api/v1/auth/passwords'
+      }
+    end
+  end
+end
---
+++ front_end/src/store/modules/user.js
@@ -0,0 +1,168 @@
+import { login, logout, getInfo } from '@/api/user'
+import {
+  setToken,
+  setClient,
+  setUid,
+  removeToken,
+  removeClient,
+  removeUid
+} from '@/utils/auth'
+import router, { resetRouter } from '@/router'
+
+const state = {
+  token: '',
+  client: '',
+  uid: '',
+  name: '',
+  avatar: '',
+  introduction: '',
+  roles: []
+}
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_CLIENT: (state, client) => {
+    state.client = client
+  },
+  SET_UID: (state, uid) => {
+    state.uid = uid
+  },
+  SET_INTRODUCTION: (state, introduction) => {
+    state.introduction = introduction
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  },
+  SET_ROLES: (state, roles) => {
+    state.roles = roles
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { email, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ email: email.trim(), password: password }).then(response => {
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // 認証情報保存
+  setAuth({ commit }, auth) {
+    return new Promise(resolve => {
+      commit('SET_TOKEN', auth['access-token'])
+      commit('SET_CLIENT', auth['client'])
+      commit('SET_UID', auth['uid'])
+      setToken(auth['access-token'])
+      setClient(auth['client'])
+      setUid(auth['uid'])
+      // debug
+      commit('SET_ROLES', ['admin', 'editor'])
+      resolve()
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token).then(response => {
+        const { data } = response
+
+        if (!data) {
+          reject('Verification failed, please Login again.')
+        }
+
+        const { roles, name, avatar, introduction } = data
+
+        // roles must be a non-empty array
+        if (!roles || roles.length <= 0) {
+          reject('getInfo: roles must be a non-null array!')
+        }
+
+        commit('SET_ROLES', roles)
+        commit('SET_NAME', name)
+        commit('SET_AVATAR', avatar)
+        commit('SET_INTRODUCTION', introduction)
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state, dispatch }) {
+    return new Promise((resolve, reject) => {
+      logout(state.token).then(() => {
+        commit('SET_TOKEN', '')
+        commit('SET_CLIENT', '')
+        commit('SET_UID', '')
+        removeToken()
+        removeClient()
+        removeUid()
+        commit('SET_ROLES', [])
+        resetRouter()
+        dispatch('tagsView/delAllViews', null, { root: true })
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      commit('SET_TOKEN', '')
+      commit('SET_CLIENT', '')
+      commit('SET_UID', '')
+      removeToken()
+      removeClient()
+      removeUid()
+      commit('SET_ROLES', [])
+      removeToken()
+      resolve()
+    })
+  },
+
+  // dynamically modify permissions
+  changeRoles({ commit, dispatch }, role) {
+    return new Promise(async resolve => {
+      const token = role + '-token'
+
+      commit('SET_TOKEN', token)
+      setToken(token)
+
+      const { roles } = await dispatch('getInfo')
+
+      resetRouter()
+
+      // generate accessible routes map based on roles
+      const accessRoutes = await dispatch('permission/generateRoutes', roles, { root: true })
+
+      // dynamically add accessible routes
+      router.addRoutes(accessRoutes)
+
+      // reset visited views and cached views
+      dispatch('tagsView/delAllViews', null, { root: true })
+
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
